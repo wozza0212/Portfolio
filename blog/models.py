dbcb7daabe165defd5010d735315cb9322af6a81
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2021-07-23 21:51:00 +0100

    update a few errors, timezone, taggit

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now())
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
--- programmer_portfolio/settings.py
+++ programmer_portfolio/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
